1bfd2d17f93f1cffb455a17d492dddbd5b91584b
DILSHODBEK <dilshodsattar26@gmail.com>
2026-06-01 06:03:15 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class authVarseWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = authVarseWorker()
+    print("Worker engine initialized.")
